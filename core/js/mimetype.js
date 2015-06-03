32c2754ea05f3f5e95c0f4558c6a2978039cd031
Roeland Jago Douma <roeland@famdouma.nl>
2015-06-03 14:52:00 +0200

    Javascript mimetype icon resolver
    This makes it possible to retrieve the icon for mimetypes in javascript.
    It makes no additional queries to the server to retrieve the mimetype.
    
    * mimetype.js: this is where the logic resides to convert from mimetype
      to icon url
    * mimetypes.js: generated file with a list of mimetype mapping (aliases)
      and the list of icon files
    * ./occ maintenance:mimetypesjs : new command for occ to gernerate
      mimetypes.js
    * unit tests updated and still work

---
+++ core/js/mimetype.js
@@ -0,0 +1,56 @@
+OC.MimeType = {
+
+	mimeTypeIcons: {},
+	
+	/**
+	 * Return the url to icon of the given mimeType
+	 *
+	 * @param {string} mimeType - The mimeType to get the icon for
+	 * @return {string} Url to the icon for mimeType
+	 */
+	getIconUrl: function(mimeType) {
+		if (_.isUndefined(mimeType)) {
+			return undefined;
+		}
+
+		if (mimeType in OC.MimeTypes.aliases) {
+			mimeType = OC.MimeTypes.aliases[mimeType];
+		}
+		if (mimeType in OC.MimeType.mimeTypeIcons) {
+			return OC.MimeType.mimeTypeIcons[mimeType];
+		}
+
+		var icon = mimeType.replace(new RegExp('/', 'g'), '-');
+
+		var path = OC.webroot + '/core/img/filetypes/';
+
+		// Generate path
+		if (mimeType === 'dir') {
+			path += 'folder';
+		} else if (mimeType === 'dir-shared') {
+			path += 'folder-shared';
+		} else if (mimeType === 'dir-external') {
+			path += 'folder-external';
+		} else if ($.inArray(icon, OC.MimeTypes.files)) {
+			path += icon;
+		} else if ($.inArray(icon.split('-')[0], OC.MimeTypes.files)) {
+			path += icon.split('-')[0];
+		} else {
+			path += 'file';
+		}
+
+		// Use svg if we can
+		if(OC.Util.hasSVGSupport()){
+			path += '.svg';
+		} else {
+			path += '.png';
+		}
+
+		// Cache the result
+		OC.MimeType.mimeTypeIcons[mimeType] = path;
+		return path;
+	}
+
+};
+
+
